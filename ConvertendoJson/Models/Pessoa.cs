34e433a4cb3d42a040a8658c0452dd90107b4981
Vidal777rj <michelvidal65@outlook.com>
2025-05-11 22:36:59 -0300

    Desafio convertendo json

---
+++ ConvertendoJson/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace ConvertendoJson.Models
+{
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+        public string? Sexo { get; set; }
+    }
+}
---
+++ ConvertendoJson/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Converter o arquivo JSON em uma lista e exibir no console*/
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using ConvertendoJson.Models;
+
+string caminhoArquivo = "nomes.json";
+ExibirDados(caminhoArquivo);
+ExbirPorIdade(caminhoArquivo);
+static void ExibirDados(string caminhoArquivo)
+{
+    try
+    {
+        var json = File.ReadAllText(caminhoArquivo);
+        List<Pessoa> dadosJson = JsonSerializer.Deserialize<List<Pessoa>>(json);
+        string jsonFormatado = JsonSerializer.Serialize(dadosJson, new JsonSerializerOptions { WriteIndented = true });
+        System.Console.WriteLine(jsonFormatado);
+
+    }
+    catch (Exception ex)
+    {
+        System.Console.WriteLine($"Erro ao converter dados: {ex.Message}");
+    }
+}
+
+static void ExbirPorIdade(string caminhoArquivo)
+{
+    try
+    {
+        var json = File.ReadAllText(caminhoArquivo);
+        List<Pessoa> nomes = JsonSerializer.Deserialize<List<Pessoa>>(json);
+
+        var filtrarPorIdade = nomes.Where(p => p.Idade > 30).ToList();
+
+        foreach (var pessoa in filtrarPorIdade)
+        {
+            System.Console.WriteLine($"{pessoa.Nome} - {pessoa.Idade}");
+        }
+
+    }
+    catch (Exception ex)
+    {
+        System.Console.WriteLine($"Erro ao converter dados: {ex.Message}");
+    }
+}
